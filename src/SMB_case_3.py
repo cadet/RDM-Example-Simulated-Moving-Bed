4ce7fd18c2da931753b045341459cce9c35ed126
Katharina Paul <katharina.paul@rwth-aachen.de>
2025-04-30 16:37:32 +0200

    add jupytext headers

--- src/SMB_case_3.py
+++ src/SMB_case_3.py
@@ -1,3 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.7
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
 # %% [markdown]
 # (Example_3_SMB)=
 # # Ternary separation
