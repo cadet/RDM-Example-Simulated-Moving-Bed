e14dd88040d052d9a464b80dd52d3be0b3f9fa1f
Katharina Paul <katharina.paul2403@gmail.com>
2026-03-03 15:45:41 +0100

    Update description in index and add image

--- src/Index.py
+++ src/Index.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.17.2
+#       jupytext_version: 1.19.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -25,39 +25,39 @@
 # %% [markdown]
 # ## Theoretical background
 #
-# Continuous processes generally have many benefits over batch processes, like higher efficiency and throughput. However, a truly continuous chromatography process is not practically feasable. This so called **true moving bed chromatography** would entail the solid phase of the chromatography column (the bed) moving in the opposite direction to the mobile phase. This would induce the local separation of components in the feed solution based on their column binding behaviour. Those components could then by retrieved by different outlet streams located upstream and downstream of the feed inlet. 
+# Continuous processes generally have many benefits over batch processes, like higher efficiency and throughput. However, a truly continuous process in chromatography is not practically feasable. <br>
+# This so called **true moving bed chromatography** would entail the solid phase of the chromatography column (the bed) moving in the opposite direction to the mobile phase. This would induce the local separation of components in the feed solution based on their column binding behaviour. Those components could then be retrieved by different outlet streams located upstream and downstream of the feed inlet.
 #
-# ```{figure} ./figures/true_MB.png
+# ```{figure} ./figures/true_moving_bed_chr.jpg
 # :width: 400px
 # <div style="text-align: center">
 #
-# Place-holder graphic from: [Link to Youtube Video](https://www.youtube.com/watch?v=xhhJxb48tgc) 
+# Graphic based on [Youtube Video by orochemtech](https://www.youtube.com/watch?v=xhhJxb48tgc) 
 # <div>
-#
-#
 
 # %% [markdown]
-# **Simulated Moving Bed Chromatography** is a way to approach such a continuous process in practice. This is realized by having multiple chromatography columns connected to each other in a carousel. By periodically switching the inlet and outlet valves connected to the columns ("column switching") the movement of the solid phase can be mimicked. 
+# **Simulated Moving Bed Chromatography** is a method to approach such a continuous process in practice. This is realized by having multiple chromatography columns connected to each other in a carousel. By periodically switching the inlet and outlet valves connected to the columns (***"column switching"***) the movement of the solid phase can be mimicked. 
 #
-# A general SMB system contains four different external units connected to the columns:
-# 1. Feed (Inlet): Component mixture
-# 2. Raffinate (Outlet): Faster eluting component (elutes before feed plug flow)
-# 3. Extract (Outlet): Slower eluting component (elutes after feed plug flow), interacts more strongly with the column solid phase<br>
+# A standard SMB system contains four different external units connected to the columns:
+# 1. **Feed** (Inlet): Component mixture
+# 2. **Raffinate** (Outlet): Faster eluting component (elutes before feed plug flow)
+# 3. **Extract** (Outlet): Slower eluting component (elutes after feed plug flow), interacts more strongly with the column solid phase<br>
 # There can be multiple extract or raffinate outlets in a SMB system depending on the number of components to be separated.
-# 4. Desorbant / Eluent / Solvent (Inlet): Solution to elute extract from column before raffinate plug flow enters the column again to prevent mixing of the separated components
+# 4. **Desorbant / Eluent / Solvent** (Inlet): Solution to elute extract from column before raffinate plug flow enters the column again to prevent mixing of the separated components
 #
-# The position of each column relative to these external units determines their specific **“Zone”** in the SMB system. Based on thier external units, every zone has a different function in the seperation process and a different flow rate within the columns. The most basic SMB system for binary separation is made up of four zones with one chromatography column in each zone. 
-#
+# The position of each column relative to these external units determines their specific **“Zone”** in the SMB system. Based on thier external units, every zone has a different function in the seperation process and a different flow rate within the columns. The most basic SMB system for binary separation is made up of four zones with one chromatography column in each zone.
 
 # %% [markdown]
 # ```{figure} ./figures/four_zone.jpg
 # :width: 600px
-# <div style="text-align: center">
-# (Fig. 1, He et al.) Schematic of four-zone SMB chromatography for binary separations. Column positions are periodically switched in opposite direction of liquid flow.
+# :align: center
+#
+# Schematic of four-zone SMB chromatography for binary separations. Column positions are periodically switched in opposite direction of liquid flow.
+# [Fig. 1, He et al.](https://www.sciencedirect.com/science/article/pii/S0098135417304520#fig0001)
 
 # %% [markdown]
 # ## Content
-# In their study He et al. examined five different cases of SMB processes. The following example reproduces case study 1, a four-zone binary separation with eight columns and case study 3, a five-zone ternary separation with five columns. 
+# The following two case studies simulate a binary separation using a four-zone SMB system and a ternary separation using a five-zone SMB system. 
 #
 # ```{toctree}
 # :maxdepth: 1
